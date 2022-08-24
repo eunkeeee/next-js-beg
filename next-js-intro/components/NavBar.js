2700cff6d826a07d942c44e6690cca8a8419d043
eunkeeee <eunkeeeee@gmail.com>
2022-08-24 13:48:20 +0900

    1.3

--- next-js-intro/components/NavBar.js
+++ next-js-intro/components/NavBar.js
@@ -1,8 +1,23 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
   return (
     <nav>
-      <a href="/">Home</a>
-      <a href="/about-us">About Us</a>
+      <Link href="/">
+        <a
+          style={{ color: router.pathname === "/" ? "red" : "blue" }}
+          className="hello"
+        >
+          Home
+        </a>
+      </Link>
+      <Link href="/about-us">
+        <a style={{ color: router.pathname === "/about-us" ? "red" : "blue" }}>
+          About Us
+        </a>
+      </Link>
     </nav>
   );
 }
