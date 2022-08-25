9defbf658a78790b4c5df7e63103c473adbe98a8
eunkeeee <eunkeeeee@gmail.com>
2022-08-25 13:57:14 +0900

    2.0 복붙을 피하기 위한 다양한 패턴

--- next-js-intro/pages/_app.js
+++ next-js-intro/pages/_app.js
@@ -1,12 +1,12 @@
+import Layout from "../components/Layout";
 import NavBar from "../components/NavBar";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
       <span>공통적으로 추가되는 부분</span>
-    </>
+    </Layout>
   );
 }
