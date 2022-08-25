d7a4c226964652ed42a13ab8b89bae7e80c69fb2
eunkeeee <eunkeeeee@gmail.com>
2022-08-25 16:34:15 +0900

    2.2 Redirect, Rewrite로 API KEY 가리기

--- next-js-intro/next.config.js
+++ next-js-intro/next.config.js
@@ -1,7 +1,31 @@
+const { redirect } = require("next/dist/server/api-utils");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
+const API_KEY = "4aef0f9cd61166075a049aaa9680cab1";
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    // redirect되기는 하지만, URL은 변하지 않아서 user은 알 수 없다..
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
--- next-js-intro/pages/_app.js
+++ next-js-intro/pages/_app.js
@@ -6,7 +6,6 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-      <span>공통적으로 추가되는 부분</span>
     </Layout>
   );
 }
